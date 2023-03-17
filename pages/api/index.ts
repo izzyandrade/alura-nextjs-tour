91a8652e345118f4d9f7f9d1e8586430d2237511
Izzy Andrade <israelcarneiro97@gmail.com>
2023-03-17 18:03:42 -0300

    feat: added API Route example with types

---
+++ pages/api/index.ts
@@ -0,0 +1,12 @@
+import type { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
+
+export type ResponseData = {
+  curso: string;
+  instrutor: string;
+};
+
+const handler: NextApiHandler = (req: NextApiRequest, res: NextApiResponse<ResponseData>) => {
+  res.status(200).json({ curso: 'next.js', instrutor: 'Dev Soutinho' });
+}
+
+export default handler;
